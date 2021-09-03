a30b6ece42d3ebf755e54f51372ceaff5a5b32e4
soterq <negatifw@gmail.com>
2021-09-03 11:08:54 +0300

    Added redirect in case if user want to join using url without to enter nickname

--- src/pages/RoomPage/RoomPage.jsx
+++ src/pages/RoomPage/RoomPage.jsx
@@ -1,9 +1,12 @@
 import React, { useContext } from "react";
 
 import TenisContext from "../../containers/context/tenis-context";
-
+import {Redirect} from "react-router";
 export default function RoomPage() {
   const ctx = useContext(TenisContext);
 
+  if(!ctx.nickName){
+    return <Redirect to={"/"}/>
+  }
   return <div>Welcome To room page {ctx.nickName}</div>;
 }
